1fe5eb49826115e7f3b598b28b0b7c3214e1f985
w02071988-glitch <w02071988@gmail.com>
2026-06-23 20:03:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,17 @@
-const CACHE = 'inspecao-v3';
-const ASSETS = ['./index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+const CACHE='inspecao-v5';
+const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>
+    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
   ));
   self.clients.claim();
 });
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch',e=>{
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() =>
-      caches.match('./index.html')
-    ))
+    caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))
   );
 });
